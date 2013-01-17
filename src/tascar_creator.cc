c0975c0549a7c975639ab50a6e13bb91604ee209
Giso Grimm <g.grimm@uni-oldenburg.de>
2013-01-17 14:53:26 +0100

    scene filefortmat writer

---
+++ src/tascar_creator.cc
@@ -0,0 +1,24 @@
+#include "scene.h"
+
+using namespace TASCAR;
+
+int main(int argc,char**argv)
+{
+    scene_t scene;
+    scene.description = "dummy scene";
+    scene.src.push_back(src_object_t());
+scene.bg_amb.push_back(bg_amb_t());
+    scene.src[0].position[0] = pos_t(1,2,3);
+    scene.src[0].position[3.7] = pos_t(4.1,5.2,6.3);
+    xml_write_scene("temp.tsc",scene," created with tascar_cretor ");
+    return 0;
+}
+
+/*
+ * Local Variables:
+ * mode: c++
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * compile-command: "make -C .."
+ * End:
+ */
