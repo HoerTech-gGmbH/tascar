b61cfbafa7bad30812ec7b0a0b0a49e4d84f587c
Giso Grimm <giso.grimm@vegri.net>
2014-08-12 16:10:39 +0200

    classes moved to namespace

--- src/osc_scene.h
+++ src/osc_scene.h
@@ -4,25 +4,33 @@
 #include "scene.h"
 #include "osc_helper.h"
 
-class route_solo_p_t {
-public:
-  TASCAR::Scene::route_t* route;
-  uint32_t* anysolo;
-};
+namespace TASCAR {
 
-class osc_scene_t : public TASCAR::Scene::scene_t, public TASCAR::osc_server_t {
-public:
-  osc_scene_t(const std::string& srv_addr, const std::string& srv_port, const std::string& cfg_file);
-  ~osc_scene_t();
-protected:
-  void add_child_methods();
-private:
-  void add_object_methods(TASCAR::Scene::object_t* o);
-  void add_route_methods(TASCAR::Scene::route_t* r);
-  void add_sound_methods(TASCAR::Scene::sound_t* s);
-  void add_diffuse_methods(TASCAR::Scene::src_diffuse_t* s);
-  std::vector<route_solo_p_t*> vprs;
-};
+  class route_solo_p_t {
+  public:
+    TASCAR::Scene::route_t* route;
+    uint32_t* anysolo;
+  };
+
+  namespace Scene {
+
+    class osc_scene_t : public TASCAR::Scene::scene_t, public TASCAR::osc_server_t {
+    public:
+      osc_scene_t(const std::string& srv_addr, const std::string& srv_port, const std::string& cfg_file);
+      ~osc_scene_t();
+    protected:
+      void add_child_methods();
+    private:
+      void add_object_methods(TASCAR::Scene::object_t* o);
+      void add_route_methods(TASCAR::Scene::route_t* r);
+      void add_sound_methods(TASCAR::Scene::sound_t* s);
+      void add_diffuse_methods(TASCAR::Scene::src_diffuse_t* s);
+      std::vector<route_solo_p_t*> vprs;
+    };
+
+  }
+
+}
 
 #endif
 
