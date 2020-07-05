9d8db0de83f988926f9baafe29069d10db89f373
Giso Grimm <giso.grimm@vegri.net>
2020-07-05 14:15:41 +0200

    bugfix of delay plugin

--- plugins/src/tascar_ap_delay.cc
+++ plugins/src/tascar_ap_delay.cc
@@ -2,23 +2,23 @@
 
 class delay_t : public TASCAR::audioplugin_base_t {
 public:
-  delay_t( const TASCAR::audioplugin_cfg_t& cfg );
-  void ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos, const TASCAR::zyx_euler_t& rot, const TASCAR::transport_t& tp);
+  delay_t(const TASCAR::audioplugin_cfg_t& cfg);
+  void ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos,
+                  const TASCAR::zyx_euler_t& rot,
+                  const TASCAR::transport_t& tp);
   void configure();
   void release();
   ~delay_t();
+
 private:
   double delay;
   uint32_t idelay;
-  TASCAR::wave_t* dline;
+  std::vector<TASCAR::wave_t*> dline;
   uint32_t pos;
 };
 
-delay_t::delay_t( const TASCAR::audioplugin_cfg_t& cfg )
-  : audioplugin_base_t( cfg ),
-    delay(1),
-    dline(NULL),
-    pos(0)
+delay_t::delay_t(const TASCAR::audioplugin_cfg_t& cfg)
+    : audioplugin_base_t(cfg), delay(1), pos(0)
 {
   GET_ATTRIBUTE(delay);
 }
@@ -26,32 +26,39 @@ delay_t::delay_t( const TASCAR::audioplugin_cfg_t& cfg )
 void delay_t::configure()
 {
   audioplugin_base_t::configure();
-  dline = new TASCAR::wave_t(std::max(1.0,f_sample*delay));
+  dline.clear();
+  for(size_t k = 0; k < n_channels; ++k)
+    dline.push_back(new TASCAR::wave_t(std::max(1.0, f_sample * delay)));
   pos = 0;
 }
 
 void delay_t::release()
 {
   audioplugin_base_t::release();
-  delete dline;
-  dline = NULL;
+  for(auto d : dline)
+    delete d;
+  dline.clear();
 }
 
-delay_t::~delay_t()
-{
-}
+delay_t::~delay_t() {}
 
-void delay_t::ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t&, const TASCAR::zyx_euler_t&, const TASCAR::transport_t& tp)
+void delay_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
+                         const TASCAR::pos_t&, const TASCAR::zyx_euler_t&,
+                         const TASCAR::transport_t& tp)
 {
-  if( dline && (delay > 0) ){
-    for(uint32_t k=0;k<chunk[0].n;++k){
-      float v(dline->d[pos]);
-      dline->d[pos] = chunk[0][k];
-      chunk[0][k] = v;
-      if( pos )
+  if(delay > 0) {
+    // first iterate over samples:
+    for(uint32_t k = 0; k < chunk[0].n; ++k) {
+      // now iterate over channels:
+      for(uint32_t c = 0; c < n_channels; ++c) {
+        float v(dline[c]->d[pos]);
+        dline[c]->d[pos] = chunk[c][k];
+        chunk[c][k] = v;
+      }
+      if(pos)
         pos--;
       else
-        pos = dline->n-1;
+        pos = dline[0]->n - 1;
     }
   }
 }
