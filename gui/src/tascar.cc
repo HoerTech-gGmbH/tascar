6283c33ff205b54b924ae96145a32b9ea14a6f30
Giso Grimm <giso.grimm@vegri.net>
2020-11-24 19:55:45 +0100

    add handler for abort/termination

--- gui/src/tascar.cc
+++ gui/src/tascar.cc
@@ -1,31 +1,42 @@
+#include "tascar.h"
+#include "tascar_glade.h"
+#include "tascar_mainwindow.h"
+#include "viewport.h"
 #include <iostream>
 #include <stdint.h>
-#include "tascar.h"
-#include "viewport.h"
-#include "tascar_mainwindow.h"
-#include "tascar_glade.h"
 
 using namespace TASCAR;
 using namespace TASCAR::Scene;
 
+tascar_window_t* win(NULL);
+
+static void sighandler(int sig)
+{
+  if(win)
+    win->hide();
+}
+
 int main(int argc, char** argv)
 {
-  setlocale(LC_ALL,"C");
+  signal(SIGABRT, &sighandler);
+  signal(SIGTERM, &sighandler);
+  signal(SIGINT, &sighandler);
+  setlocale(LC_ALL, "C");
   int nargv(1);
-  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(nargv, argv, "de.hoertech.tascar",Gio::APPLICATION_NON_UNIQUE);
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(
+      nargv, argv, "de.hoertech.tascar", Gio::APPLICATION_NON_UNIQUE);
   Glib::RefPtr<Gtk::Builder> refBuilder;
-  tascar_window_t* win(NULL);
   refBuilder = Gtk::Builder::create();
   refBuilder->add_from_string(ui_tascar);
   refBuilder->get_widget_derived("MainWindow", win);
-  if( !win )
+  if(!win)
     throw TASCAR::ErrMsg("No main window");
   win->show();
-  if( argc > 1 ){
-    try{
+  if(argc > 1) {
+    try {
       win->load(argv[1]);
     }
-    catch(const std::exception& e){
+    catch(const std::exception& e) {
       std::string msg("While loading file \"");
       msg += argv[1];
       msg += "\": ";
