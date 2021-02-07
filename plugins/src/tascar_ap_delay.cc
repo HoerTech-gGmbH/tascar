4a62a36955837fd9321f15035ecb72f36a4ffcac
Giso Grimm <giso.grimm@vegri.net>
2021-02-07 15:07:01 +0100

    multi-channel delay and metronome

--- plugins/src/tascar_ap_delay.cc
+++ plugins/src/tascar_ap_delay.cc
@@ -11,25 +11,32 @@ public:
   ~delay_t();
 
 private:
-  double delay;
+  std::vector<double> delay;
   uint32_t idelay;
   std::vector<TASCAR::wave_t*> dline;
-  uint32_t pos;
+  std::vector<uint32_t> pos;
 };
 
 delay_t::delay_t(const TASCAR::audioplugin_cfg_t& cfg)
-    : audioplugin_base_t(cfg), delay(1), pos(0)
+    : audioplugin_base_t(cfg), delay({1.0}), pos(0)
 {
   GET_ATTRIBUTE(delay);
+  if(delay.empty())
+    delay.push_back(0.0);
 }
 
 void delay_t::configure()
 {
   audioplugin_base_t::configure();
   dline.clear();
-  for(size_t k = 0; k < n_channels; ++k)
-    dline.push_back(new TASCAR::wave_t(std::max(1.0, f_sample * delay)));
-  pos = 0;
+  for(size_t k = 0; k < n_channels; ++k) {
+    if(delay[k % delay.size()] > 0.0)
+      dline.push_back(new TASCAR::wave_t(
+          std::max(1.0, f_sample * delay[k % delay.size()])));
+    else
+      dline.push_back(NULL);
+  }
+  pos = std::vector<uint32_t>(n_channels, 0);
 }
 
 void delay_t::release()
@@ -46,19 +53,19 @@ void delay_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
                          const TASCAR::pos_t&, const TASCAR::zyx_euler_t&,
                          const TASCAR::transport_t& tp)
 {
-  if(delay > 0) {
-    // first iterate over samples:
-    for(uint32_t k = 0; k < chunk[0].n; ++k) {
-      // now iterate over channels:
-      for(uint32_t c = 0; c < n_channels; ++c) {
-        float v(dline[c]->d[pos]);
-        dline[c]->d[pos] = chunk[c][k];
+  // first iterate over samples:
+  for(uint32_t k = 0; k < chunk[0].n; ++k) {
+    // now iterate over channels:
+    for(uint32_t c = 0; c < n_channels; ++c) {
+      if(dline[c]) {
+        float v(dline[c]->d[pos[c]]);
+        dline[c]->d[pos[c]] = chunk[c][k];
         chunk[c][k] = v;
+        if(pos[c])
+          pos[c]--;
+        else
+          pos[c] = dline[c]->n - 1;
       }
-      if(pos)
-        pos--;
-      else
-        pos = dline[0]->n - 1;
     }
   }
 }
--- plugins/src/tascar_ap_metronome.cc
+++ plugins/src/tascar_ap_metronome.cc
@@ -1,25 +1,27 @@
 #include "audioplugin.h"
 #include "filterclass.h"
 
-
-static int osc_set_message(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data)
+static int osc_set_message(const char* path, const char* types, lo_arg** argv,
+                           int argc, lo_message msg, void* user_data)
 {
-  if( user_data ){
+  if(user_data) {
     TASCAR::msg_t* tmsg((TASCAR::msg_t*)user_data);
-    lo_message_free( tmsg->msg );
-    tmsg->msg = lo_message_clone( msg );
+    lo_message_free(tmsg->msg);
+    tmsg->msg = lo_message_clone(msg);
   }
   return 0;
 }
 
 class metronome_t : public TASCAR::audioplugin_base_t {
 public:
-  metronome_t( const TASCAR::audioplugin_cfg_t& cfg );
-  void ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos, const TASCAR::zyx_euler_t&, const TASCAR::transport_t& tp);
-  void add_variables( TASCAR::osc_server_t* srv );
+  metronome_t(const TASCAR::audioplugin_cfg_t& cfg);
+  void ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos,
+                  const TASCAR::zyx_euler_t&, const TASCAR::transport_t& tp);
+  void add_variables(TASCAR::osc_server_t* srv);
   void configure();
   void release();
   ~metronome_t();
+
 private:
   void update_par();
   bool changeonone;
@@ -47,29 +49,12 @@ private:
   TASCAR::msg_t msg;
 };
 
-metronome_t::metronome_t( const TASCAR::audioplugin_cfg_t& cfg )
-  : audioplugin_base_t( cfg ),
-    changeonone(false),
-    bpm(120),
-    bpb(4),
-    a1(0.002),
-    ao(0.001),
-    fres1(1000),
-    freso(600),
-    q1(0.997),
-    qo(0.997),
-    sync(false),
-    bypass(false),
-    bypass_(false),
-    t(0),
-    beat(0),
-    bpb_(4),
-    a1_(0.002),
-    ao_(0.001),
-    period(0),
-    dispatchin(0),
-    srv_(NULL),
-    msg( find_or_add_child( "msg" ) )
+metronome_t::metronome_t(const TASCAR::audioplugin_cfg_t& cfg)
+    : audioplugin_base_t(cfg), changeonone(false), bpm(120), bpb(4), a1(0.002),
+      ao(0.001), fres1(1000), freso(600), q1(0.997), qo(0.997), sync(false),
+      bypass(false), bypass_(false), t(0), beat(0), bpb_(4), a1_(0.002),
+      ao_(0.001), period(0), dispatchin(0), srv_(NULL),
+      msg(find_or_add_child("msg"))
 {
   GET_ATTRIBUTE_BOOL(changeonone);
   GET_ATTRIBUTE(bpm);
@@ -84,7 +69,6 @@ metronome_t::metronome_t( const TASCAR::audioplugin_cfg_t& cfg )
   GET_ATTRIBUTE_BOOL(bypass);
 }
 
-
 void metronome_t::configure()
 {
   audioplugin_base_t::configure();
@@ -96,100 +80,106 @@ void metronome_t::release()
   audioplugin_base_t::release();
 }
 
-
-void metronome_t::add_variables( TASCAR::osc_server_t* srv )
+void metronome_t::add_variables(TASCAR::osc_server_t* srv)
 {
   srv_ = srv;
-  srv->add_bool( "/changeonone", &changeonone );
-  srv->add_double( "/bpm", &bpm );
-  srv->add_uint( "/bpb", &bpb );
-  srv->add_double_dbspl( "/a1", &a1 );
-  srv->add_double_dbspl( "/ao", &ao );
-  srv->add_bool( "/sync", &sync );
-  srv->add_double( "/filter/f1", &fres1 );
-  srv->add_double( "/filter/fo", &freso );
-  srv->add_double( "/filter/q1", &q1 );
-  srv->add_double( "/filter/qo", &qo );
-  srv->add_bool("/bypass", &bypass );
-  srv->add_uint("/dispatchin", &dispatchin );
-  srv->add_method( "/dispatchmsg", NULL, &osc_set_message, &msg );
-  srv->add_string( "/dispatchpath", &(msg.path) );
+  srv->add_bool("/changeonone", &changeonone);
+  srv->add_double("/bpm", &bpm);
+  srv->add_uint("/bpb", &bpb);
+  srv->add_double_dbspl("/a1", &a1);
+  srv->add_double_dbspl("/ao", &ao);
+  srv->add_bool("/sync", &sync);
+  srv->add_double("/filter/f1", &fres1);
+  srv->add_double("/filter/fo", &freso);
+  srv->add_double("/filter/q1", &q1);
+  srv->add_double("/filter/qo", &qo);
+  srv->add_bool("/bypass", &bypass);
+  srv->add_uint("/dispatchin", &dispatchin);
+  srv->add_method("/dispatchmsg", NULL, &osc_set_message, &msg);
+  srv->add_string("/dispatchpath", &(msg.path));
 }
 
-metronome_t::~metronome_t()
-{
-}
+metronome_t::~metronome_t() {}
 
 void metronome_t::update_par()
 {
   bypass_ = bypass;
-  f1.set_fq( fres1*t_sample, q1 );
-  fo.set_fq( freso*t_sample, qo );
-  if( bpm < 6.9444e-04 )
+  f1.set_fq(fres1 * t_sample, q1);
+  fo.set_fq(freso * t_sample, qo);
+  if(bpm < 6.9444e-04)
     bpm = 6.9444e-04;
-  period = (60*(int64_t)f_sample)/(int64_t)bpm;
+  period = (60 * (int64_t)f_sample) / (int64_t)bpm;
   ao_ = ao;
   a1_ = a1;
   bpb_ = bpb;
 }
 
-void metronome_t::ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos, const TASCAR::zyx_euler_t&, const TASCAR::transport_t& tp)
+void metronome_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
+                             const TASCAR::pos_t& pos,
+                             const TASCAR::zyx_euler_t&,
+                             const TASCAR::transport_t& tp)
 {
   // apply parameters:
-  if( !changeonone )
+  if(!changeonone)
     update_par();
   float v(0);
-  TASCAR::wave_t& aud(chunk[0]);
+  size_t Nch(chunk.size());
+  uint32_t Nsmp(0);
+  if(Nch > 0)
+    Nsmp = chunk[0].n;
+  // TASCAR::wave_t& aud(chunk[0]);
   int64_t objecttime(tp.object_time_samples);
-  for(uint32_t k=0;k<aud.n;++k){
-    if( sync ){
+  for(uint32_t k = 0; k < Nsmp; ++k) {
+    if(sync) {
       // synchronize to time line:
       t = objecttime;
-      if( tp.rolling )
+      if(tp.rolling)
         ++objecttime;
-      ldiv_t tmp(ldiv( t, period ));
+      ldiv_t tmp(ldiv(t, period));
       t = tmp.rem;
       beat = tmp.quot;
-      if( bpb_ > 0 ){
-        tmp = ldiv(beat,bpb_);
+      if(bpb_ > 0) {
+        tmp = ldiv(beat, bpb_);
         beat = tmp.rem;
-      }else
+      } else
         beat = 0;
-    }else{
+    } else {
       // free-run mode, in
-      if( t >= period ){
+      if(t >= period) {
         t = 0;
         ++beat;
-        if( beat >= bpb_ )
+        if(beat >= bpb_)
           beat = 0;
       }
     }
-    if( t || (sync && !tp.rolling) ){
+    if(t || (sync && !tp.rolling)) {
       // this is not a beat sample:
       v = 0.0f;
-    }else{
+    } else {
       // now we are exactly on a beat.
       // if beat != 0 then this is not a one:
-      if( beat )
+      if(beat)
         v = ao_;
-      else{
-	if( dispatchin ){
-	  // count down "dispatchin". If zero, then dispatch message.
-	  --dispatchin;
-	  if( (!dispatchin) && srv_ )
-	    srv_->dispatch_data_message( msg.path.c_str(), msg.msg );
-	}
-        if( changeonone )
+      else {
+        if(dispatchin) {
+          // count down "dispatchin". If zero, then dispatch message.
+          --dispatchin;
+          if((!dispatchin) && srv_)
+            srv_->dispatch_data_message(msg.path.c_str(), msg.msg);
+        }
+        if(changeonone)
           update_par();
         v = a1_;
       }
     }
-    if( !bypass_ ){
+    if(!bypass_) {
       // beat-dependent filter properties:
-      if( beat )
-        aud.d[k] += fo.filter_unscaled( v );
+      if(beat)
+        v = fo.filter_unscaled(v);
       else
-        aud.d[k] += f1.filter_unscaled( v );
+        v = f1.filter_unscaled(v);
+      for(uint32_t c = 0; c < Nch; c++)
+        chunk[c].d[k] += v;
     }
     ++t;
   }
