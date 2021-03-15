1a4b56df975fbbfeb6212a9f608d7af56dff1db7
delude88 <tobias.hegemann@googlemail.com>
2021-03-15 15:50:21 +0100

    Fixed version.cmake

--- version.cmake
+++ version.cmake
@@ -8,8 +8,9 @@ set(PROJECT_VERSION_PATCH ${CMAKE_MATCH_3})
 
 # Now read git related
 execute_process(COMMAND git log --pretty=format:'%h' -n 1
-                OUTPUT_VARIABLE GIT_REV
-                ERROR_QUIET)
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        OUTPUT_VARIABLE GIT_REV
+        ERROR_QUIET)
 
 # Check whether we got any revision (which isn't
 # always the case, e.g. when someone downloaded a zip
@@ -20,24 +21,29 @@ if ("${GIT_REV}" STREQUAL "")
     set(GIT_TAG "N/A")
     set(GIT_BRANCH "N/A")
     set(GIT_NUM_COMMITS "0")
-else()
+else ()
     execute_process(
-        COMMAND bash -c "git diff --quiet --exit-code || echo modified"
-        OUTPUT_VARIABLE GIT_DIFF)
+            COMMAND bash -c "git diff --quiet --exit-code || echo modified"
+            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            OUTPUT_VARIABLE GIT_DIFF)
     execute_process(
-        COMMAND git describe --exact-match --tags
-        OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
+            COMMAND git describe --exact-match --tags
+            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
     execute_process(
-        COMMAND git rev-parse --abbrev-ref HEAD
-        OUTPUT_VARIABLE GIT_BRANCH)
+            COMMAND git rev-parse --abbrev-ref HEAD
+            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            OUTPUT_VARIABLE GIT_BRANCH)
 
-    if(WIN32)
+    if (WIN32)
         execute_process(COMMAND bash -c "git log --pretty='format:%h' origin/master.. | find /c /v ''"
-            OUTPUT_VARIABLE GIT_NUM_COMMITS)
-    else()
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                OUTPUT_VARIABLE GIT_NUM_COMMITS)
+    else ()
         execute_process(COMMAND bash -c "git log --pretty='format:%h' origin/master.. | wc -w"
-            OUTPUT_VARIABLE GIT_NUM_COMMITS)
-    endif()
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                OUTPUT_VARIABLE GIT_NUM_COMMITS)
+    endif ()
 
     string(STRIP "${GIT_REV}" GIT_REV)
     string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
@@ -45,7 +51,7 @@ else()
     string(STRIP "${GIT_TAG}" GIT_TAG)
     string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
     string(STRIP "${GIT_NUM_COMMITS}" GIT_NUM_COMMITS)
-endif()
+endif ()
 
 set(VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
 set(FULLVERSION "${VERSION}.${GIT_NUM_COMMITS}-${GIT_REV}${$GIT_DIFF}")
