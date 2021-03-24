7c6558431cd58b3a1a8ba60db71b5c4d3e25307e
Giso Grimm <giso.grimm@vegri.net>
2021-03-24 09:39:53 +0100

    allow configuration of jack name

--- plugins/src/tascarmod_waitforjackport.cc
+++ plugins/src/tascarmod_waitforjackport.cc
@@ -14,17 +14,19 @@ private:
 wfjp_t::wfjp_t(const TASCAR::module_cfg_t& cfg)
     : module_base_t(cfg), timeout(30)
 {
+  std::string name("waitforjackport");
   GET_ATTRIBUTE_(ports);
   GET_ATTRIBUTE_(timeout);
+  GET_ATTRIBUTE_(name);
   for( auto sne : tsccfg::node_get_children(e,"port")){
     ports.push_back(tsccfg::node_get_text(sne, ""));
   }
   jack_status_t jstat;
   jack_client_t* jc;
   jack_options_t opt = (jack_options_t)(JackNoStartServer | JackUseExactName);
-  jc = jack_client_open("waitforjackport", opt, &jstat);
+  jc = jack_client_open(name.c_str(), opt, &jstat);
   if(!jc)
-    throw TASCAR::ErrMsg("Unable to create jack client \"jackwaitforport\".");
+    throw TASCAR::ErrMsg("Unable to create jack client \""+name+"\".");
   for(auto p : ports) {
     uint32_t k(timeout * 100);
     jack_port_t* jp = NULL;
