d32807dbcd7f91a1b68d06c81d32b5527a9eacc9
Giso Grimm <giso.grimm@vegri.net>
2022-02-02 17:14:08 +0100

    allow remote control of sndfile plugin

--- plugins/src/tascar_ap_sndfile.cc
+++ plugins/src/tascar_ap_sndfile.cc
@@ -25,6 +25,7 @@
 #include "errorhandling.h"
 #include <fstream>
 #include <thread>
+#include <mutex>
 
 class ap_sndfile_cfg_t : public TASCAR::audioplugin_base_t {
 public:
@@ -88,107 +89,137 @@ public:
   void ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos, const TASCAR::zyx_euler_t& , const TASCAR::transport_t& tp);
   void add_variables( TASCAR::osc_server_t* srv );
   void add_licenses( licensehandler_t* session );
+  void load_file();
+  void unload_file();
   void configure();
   void release();
 private:
+  static int osc_loadfile(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data);
+  void osc_loadfile(const std::string& fname, const std::string& lmode, float level);
   uint32_t triggeredloop;
   TASCAR::transport_t ltp;
   std::vector<TASCAR::sndfile_t*> sndf;
+  std::mutex mtx;
 };
 
-ap_sndfile_t::ap_sndfile_t( const TASCAR::audioplugin_cfg_t& cfg )
-  : ap_sndfile_cfg_t( cfg ),
-    triggeredloop(0)
+ap_sndfile_t::ap_sndfile_t(const TASCAR::audioplugin_cfg_t& cfg)
+    : ap_sndfile_cfg_t(cfg), triggeredloop(0)
 {
-  get_license_info( e, name, license, attribution );
+  get_license_info(e, name, license, attribution);
 }
 
 void ap_sndfile_t::configure()
 {
   TASCAR::audioplugin_base_t::configure();
-  if(n_channels < 1)
-    throw TASCAR::ErrMsg("At least one channel required.");
-  sndf.clear();
-  if((n_channels == 4) && (channelorder != "none")) {
-    // probably FOA, check for channelorder
-    if(channelorder.empty()) {
-      TASCAR::add_warning(
-          "No channel order is specified, but probably FOA format. Please "
-          "specify ``FuMa'', ``ACN'' or ``none''.",
-          e);
-    }
-    if((channelorder == "FuMa") || channelorder.empty()) {
-      sndf.push_back(new TASCAR::sndfile_t(name, 0, start, length));
-      sndf.push_back(new TASCAR::sndfile_t(name, 2, start, length));
-      sndf.push_back(new TASCAR::sndfile_t(name, 3, start, length));
-      sndf.push_back(new TASCAR::sndfile_t(name, 1, start, length));
-    } else if(channelorder == "ACN") {
-      sndf.push_back(new TASCAR::sndfile_t(name, 0, start, length));
-      sndf.push_back(new TASCAR::sndfile_t(name, 1, start, length));
-      sndf.push_back(new TASCAR::sndfile_t(name, 2, start, length));
-      sndf.push_back(new TASCAR::sndfile_t(name, 3, start, length));
-    } else
-      throw TASCAR::ErrMsg("Invalid channel order: \"" + channelorder + "\"");
-  } else {
-    for(uint32_t ch = 0; ch < n_channels; ++ch) {
-      sndf.push_back(new TASCAR::sndfile_t(name, channel + ch, start, length));
-    }
-  }
-  if(sndf[0]->get_srate() != f_sample) {
-    double origsrate(sndf[0]->get_srate());
-    if(resample) {
-      std::vector<std::thread*> threads;
-      for(auto sf : sndf){
-        threads.push_back(new std::thread(&TASCAR::sndfile_t::resample,sf,f_sample / origsrate));
-        //sf->resample(f_sample / origsrate);
-      }
-      for(auto th : threads ){
-        th->join();
-        delete th;
-      }
-    } else {
-      std::string msg("The sample rate of the sound file " + name +
-                      " differs from the audio system sample rate: ");
-      char ctmp[1024];
-      sprintf(ctmp, "file has %d Hz, expected %g Hz", sndf[0]->get_srate(),
-              f_sample);
-      msg += ctmp;
-      TASCAR::add_warning(msg, e);
-    }
-  }
-  double gain(1);
-  if(levelmode == "rms") {
-    TASCAR::levelmeter_t meter(f_sample, sndf[0]->n / f_sample, weighting);
-    meter.update(*(sndf[0]));
-    gain = level * 2e-5 / meter.rms();
-  } else if(levelmode == "peak") {
-    float maxabs(0);
-    for(auto sf = sndf.begin(); sf != sndf.end(); ++sf)
-      maxabs = std::max(maxabs, (*sf)->maxabs());
-    if(maxabs > 0)
-      gain = level * 2e-5 / maxabs;
-  } else if(levelmode == "calib")
-    gain = level * 2e-5;
-  else
-    throw TASCAR::ErrMsg("Invalid level mode \"" + levelmode + "\". (sndfile)");
-  for(auto sf = sndf.begin(); sf != sndf.end(); ++sf) {
-    if(triggered) {
-      (*sf)->set_position(-((*sf)->n) * ((*sf)->get_srate()));
-      (*sf)->set_loop(1);
-    } else {
-      (*sf)->set_position(position);
-      (*sf)->set_loop(loop);
-    }
-    *(*sf) *= gain;
-  }
+  load_file();
 }
 
-void ap_sndfile_t::release(  )
+void ap_sndfile_t::load_file()
+{
+  mtx.lock();
+  sndf.clear();
+  try {
+    if(n_channels < 1)
+      throw TASCAR::ErrMsg("At least one channel required.");
+    if(name.size()) {
+      if((n_channels == 4) && (channelorder != "none")) {
+        // probably FOA, check for channelorder
+        if(channelorder.empty()) {
+          TASCAR::add_warning(
+              "No channel order is specified, but probably FOA format. Please "
+              "specify ``FuMa'', ``ACN'' or ``none''.",
+              e);
+        }
+        if((channelorder == "FuMa") || channelorder.empty()) {
+          sndf.push_back(new TASCAR::sndfile_t(name, 0, start, length));
+          sndf.push_back(new TASCAR::sndfile_t(name, 2, start, length));
+          sndf.push_back(new TASCAR::sndfile_t(name, 3, start, length));
+          sndf.push_back(new TASCAR::sndfile_t(name, 1, start, length));
+        } else if(channelorder == "ACN") {
+          sndf.push_back(new TASCAR::sndfile_t(name, 0, start, length));
+          sndf.push_back(new TASCAR::sndfile_t(name, 1, start, length));
+          sndf.push_back(new TASCAR::sndfile_t(name, 2, start, length));
+          sndf.push_back(new TASCAR::sndfile_t(name, 3, start, length));
+        } else
+          throw TASCAR::ErrMsg("Invalid channel order: \"" + channelorder +
+                               "\"");
+      } else {
+        for(uint32_t ch = 0; ch < n_channels; ++ch) {
+          sndf.push_back(
+              new TASCAR::sndfile_t(name, channel + ch, start, length));
+        }
+      }
+      if(sndf[0]->get_srate() != f_sample) {
+        double origsrate(sndf[0]->get_srate());
+        if(resample) {
+          std::vector<std::thread*> threads;
+          for(auto sf : sndf) {
+            threads.push_back(new std::thread(&TASCAR::sndfile_t::resample, sf,
+                                              f_sample / origsrate));
+            // sf->resample(f_sample / origsrate);
+          }
+          for(auto th : threads) {
+            th->join();
+            delete th;
+          }
+        } else {
+          std::string msg("The sample rate of the sound file " + name +
+                          " differs from the audio system sample rate: ");
+          char ctmp[1024];
+          sprintf(ctmp, "file has %d Hz, expected %g Hz", sndf[0]->get_srate(),
+                  f_sample);
+          msg += ctmp;
+          TASCAR::add_warning(msg, e);
+        }
+      }
+      double gain(1);
+      if(levelmode == "rms") {
+        TASCAR::levelmeter_t meter(f_sample, sndf[0]->n / f_sample, weighting);
+        meter.update(*(sndf[0]));
+        gain = level * 2e-5 / meter.rms();
+      } else if(levelmode == "peak") {
+        float maxabs(0);
+        for(auto sf = sndf.begin(); sf != sndf.end(); ++sf)
+          maxabs = std::max(maxabs, (*sf)->maxabs());
+        if(maxabs > 0)
+          gain = level * 2e-5 / maxabs;
+      } else if(levelmode == "calib")
+        gain = level * 2e-5;
+      else
+        throw TASCAR::ErrMsg("Invalid level mode \"" + levelmode +
+                             "\". (sndfile)");
+      for(auto sf = sndf.begin(); sf != sndf.end(); ++sf) {
+        if(triggered) {
+          (*sf)->set_position(-((*sf)->n) * ((*sf)->get_srate()));
+          (*sf)->set_loop(1);
+        } else {
+          (*sf)->set_position(position);
+          (*sf)->set_loop(loop);
+        }
+        *(*sf) *= gain;
+      }
+    }
+  }
+  catch(...) {
+    mtx.unlock();
+    throw;
+  }
+  mtx.unlock();
+}
+
+void ap_sndfile_t::release()
 {
   TASCAR::audioplugin_base_t::release();
-  for( auto it=sndf.begin();it!=sndf.end();++it)
-    delete (*it);
+  unload_file();
+}
+
+void ap_sndfile_t::unload_file()
+{
+  mtx.lock();
+  for(auto it = sndf.begin(); it != sndf.end(); ++it)
+    delete(*it);
   sndf.clear();
+  mtx.unlock();
 }
 
 ap_sndfile_t::~ap_sndfile_t()
@@ -198,7 +229,32 @@ ap_sndfile_t::~ap_sndfile_t()
 void ap_sndfile_t::add_licenses( licensehandler_t* session )
 {
   audioplugin_base_t::add_licenses( session );
+  if( name.size() )
   session->add_license( license, attribution, TASCAR::tscbasename(TASCAR::env_expand(name)) );
+}
+
+int ap_sndfile_t::osc_loadfile(const char *path, const char *types, lo_arg **argv, int argc, lo_message msg, void *user_data)
+{
+  if( user_data  )
+    ((ap_sndfile_t*)user_data)->osc_loadfile(&(argv[0]->s),&(argv[1]->s),argv[2]->f);
+  return 0;
+}
+
+void ap_sndfile_t::osc_loadfile(const std::string& fname,
+                                const std::string& lmode, float nlevel)
+{
+  mtx.lock();
+  name = fname;
+  levelmode = lmode;
+  level = powf(10.0f, 0.05f * nlevel);
+  mtx.unlock();
+  try {
+    unload_file();
+    load_file();
+  }
+  catch(const std::exception& e) {
+    TASCAR::add_warning(std::string("Error while loading file: ") + e.what());
+  }
 }
 
 void ap_sndfile_t::add_variables( TASCAR::osc_server_t* srv )
@@ -206,27 +262,36 @@ void ap_sndfile_t::add_variables( TASCAR::osc_server_t* srv )
   if( triggered )
     srv->add_uint( "/loop", &triggeredloop );
   srv->add_bool( "/mute", &mute );
+  srv->add_method("/loadfile","ssf",&osc_loadfile,this);
 }
 
-void ap_sndfile_t::ap_process(std::vector<TASCAR::wave_t>& chunk, const TASCAR::pos_t& pos, const TASCAR::zyx_euler_t& , const TASCAR::transport_t& tp)
+void ap_sndfile_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
+                              const TASCAR::pos_t& pos,
+                              const TASCAR::zyx_euler_t&,
+                              const TASCAR::transport_t& tp)
 {
-  if( transport )
-    ltp = tp;
-  if( triggered ){
-    if( triggeredloop ){
-      for( auto sf=sndf.begin();sf!=sndf.end();++sf){
-        (*sf)->set_iposition(ltp.object_time_samples);
-        (*sf)->set_loop(triggeredloop);
+  if(mtx.try_lock()) {
+    if(sndf.size()) {
+      if(transport)
+        ltp = tp;
+      if(triggered) {
+        if(triggeredloop) {
+          for(auto sf = sndf.begin(); sf != sndf.end(); ++sf) {
+            (*sf)->set_iposition(ltp.object_time_samples);
+            (*sf)->set_loop(triggeredloop);
+          }
+          triggeredloop = 0;
+        }
       }
-      triggeredloop = 0;
+      if((!mute) && (tp.rolling || (!transport))) {
+        for(uint32_t ch = 0; ch < std::min(sndf.size(), chunk.size()); ++ch)
+          sndf[ch]->add_to_chunk(ltp.object_time_samples, chunk[ch]);
+      }
+      if(!transport)
+        ltp.object_time_samples += chunk[0].n;
     }
+    mtx.unlock();
   }
-  if( (!mute) && (tp.rolling || (!transport)) ){
-    for( uint32_t ch=0;ch<std::min(sndf.size(),chunk.size());++ch)
-      sndf[ch]->add_to_chunk( ltp.object_time_samples, chunk[ch] );
-  }
-  if( !transport)
-    ltp.object_time_samples += chunk[0].n;
 }
 
 REGISTER_AUDIOPLUGIN(ap_sndfile_t);
