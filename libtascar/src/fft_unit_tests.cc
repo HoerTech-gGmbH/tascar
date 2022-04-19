ae4397111cd0228b5cd42eb06364d4da506d0f9c
Giso Grimm <giso.grimm@vegri.net>
2022-04-19 18:24:01 +0200

    adapt test code to new bandlevels interface

--- libtascar/src/fft_unit_tests.cc
+++ libtascar/src/fft_unit_tests.cc
@@ -22,7 +22,7 @@
 
 #include "fft.h"
 
-TEST(fft_t,fftifft)
+TEST(fft_t, fftifft)
 {
   TASCAR::wave_t w(4);
   TASCAR::fft_t fft(4);
@@ -32,17 +32,17 @@ TEST(fft_t,fftifft)
   w.d[3] = -2;
   // transform to spectrum:
   fft.execute(w);
-  ASSERT_NEAR(fft.s.b[0].real(),-5.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[0].imag(),0.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[1].real(),-2.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[1].imag(),-1.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[2].real(),1.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[2].imag(),0.0f,1e-7f);
+  ASSERT_NEAR(fft.s.b[0].real(), -5.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[0].imag(), 0.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[1].real(), -2.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[1].imag(), -1.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[2].real(), 1.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[2].imag(), 0.0f, 1e-7f);
   fft.ifft();
-  ASSERT_NEAR(fft.w.d[0],-2.0f,1e-7f);
-  ASSERT_NEAR(fft.w.d[1],-1.0f,1e-7f);
-  ASSERT_NEAR(fft.w.d[2],0.0f,1e-7f);
-  ASSERT_NEAR(fft.w.d[3],-2.0f,1e-7f);
+  ASSERT_NEAR(fft.w.d[0], -2.0f, 1e-7f);
+  ASSERT_NEAR(fft.w.d[1], -1.0f, 1e-7f);
+  ASSERT_NEAR(fft.w.d[2], 0.0f, 1e-7f);
+  ASSERT_NEAR(fft.w.d[3], -2.0f, 1e-7f);
 }
 
 TEST(fft_t, hilbert)
@@ -53,20 +53,20 @@ TEST(fft_t, hilbert)
   w.d[0] = 1;
   w.d[1] = 1;
   fft.hilbert(w);
-  EXPECT_EQ(fft.w.d[0],-0.5);
-  EXPECT_EQ(fft.w.d[1],0.5);
-  EXPECT_EQ(fft.w.d[2],0.5);
-  EXPECT_EQ(fft.w.d[3],-0.5);
+  EXPECT_EQ(fft.w.d[0], -0.5);
+  EXPECT_EQ(fft.w.d[1], 0.5);
+  EXPECT_EQ(fft.w.d[2], 0.5);
+  EXPECT_EQ(fft.w.d[3], -0.5);
   // example 2: -2,-1,0,-2
   w.d[0] = -2;
   w.d[1] = -1;
   w.d[2] = 0;
   w.d[3] = -2;
   fft.hilbert(w);
-  EXPECT_EQ(fft.w.d[0],-0.5);
-  EXPECT_EQ(fft.w.d[1],-1.0);
-  EXPECT_EQ(fft.w.d[2],0.5);
-  EXPECT_EQ(fft.w.d[3],1.0);
+  EXPECT_EQ(fft.w.d[0], -0.5);
+  EXPECT_EQ(fft.w.d[1], -1.0);
+  EXPECT_EQ(fft.w.d[2], 0.5);
+  EXPECT_EQ(fft.w.d[3], 1.0);
   // example 3: 1,1,0,0,1,1,0,0
   TASCAR::wave_t w2(8);
   TASCAR::fft_t fft2(8);
@@ -75,14 +75,14 @@ TEST(fft_t, hilbert)
   w2.d[4] = 1;
   w2.d[5] = 1;
   fft2.hilbert(w2);
-  EXPECT_EQ(fft2.w.d[0],-0.5);
-  EXPECT_EQ(fft2.w.d[1],0.5);
-  EXPECT_EQ(fft2.w.d[2],0.5);
-  EXPECT_EQ(fft2.w.d[3],-0.5);
-  EXPECT_EQ(fft2.w.d[4],-0.5);
-  EXPECT_EQ(fft2.w.d[5],0.5);
-  EXPECT_EQ(fft2.w.d[6],0.5);
-  EXPECT_EQ(fft2.w.d[7],-0.5);
+  EXPECT_EQ(fft2.w.d[0], -0.5);
+  EXPECT_EQ(fft2.w.d[1], 0.5);
+  EXPECT_EQ(fft2.w.d[2], 0.5);
+  EXPECT_EQ(fft2.w.d[3], -0.5);
+  EXPECT_EQ(fft2.w.d[4], -0.5);
+  EXPECT_EQ(fft2.w.d[5], 0.5);
+  EXPECT_EQ(fft2.w.d[6], 0.5);
+  EXPECT_EQ(fft2.w.d[7], -0.5);
 }
 
 TEST(minphase_t, minphase)
@@ -95,28 +95,28 @@ TEST(minphase_t, minphase)
   w.d[3] = -2;
   // transform to spectrum:
   fft.execute(w);
-  ASSERT_NEAR(fft.s.b[0].real(),-5.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[0].imag(),0.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[1].real(),-2.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[1].imag(),-1.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[2].real(),1.0f,1e-7f);
-  ASSERT_NEAR(fft.s.b[2].imag(),0.0f,1e-7f);
+  ASSERT_NEAR(fft.s.b[0].real(), -5.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[0].imag(), 0.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[1].real(), -2.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[1].imag(), -1.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[2].real(), 1.0f, 1e-7f);
+  ASSERT_NEAR(fft.s.b[2].imag(), 0.0f, 1e-7f);
   TASCAR::minphase_t minphase(4);
-  ASSERT_NEAR(std::abs(fft.s.b[0]),5.0f,1e-7f);
-  ASSERT_NEAR(std::abs(fft.s.b[1]),2.2361f,1e-4f);
-  ASSERT_NEAR(std::abs(fft.s.b[2]),1.0f,1e-7f);
+  ASSERT_NEAR(std::abs(fft.s.b[0]), 5.0f, 1e-7f);
+  ASSERT_NEAR(std::abs(fft.s.b[1]), 2.2361f, 1e-4f);
+  ASSERT_NEAR(std::abs(fft.s.b[2]), 1.0f, 1e-7f);
   minphase(fft.s);
-  ASSERT_NEAR(fft.s.b[0].real(),4.60070f,1e-5f);
-  ASSERT_NEAR(fft.s.b[0].imag(),1.95795f,1e-5f);
-  ASSERT_NEAR(fft.s.b[1].real(),1.55030f,1e-5f);
-  ASSERT_NEAR(fft.s.b[1].imag(),-1.61139f,1e-5f);
-  ASSERT_NEAR(fft.s.b[2].real(),0.92014f,1e-5f);
-  ASSERT_NEAR(fft.s.b[2].imag(),-0.39159f,1e-5f);
+  ASSERT_NEAR(fft.s.b[0].real(), 4.60070f, 1e-5f);
+  ASSERT_NEAR(fft.s.b[0].imag(), 1.95795f, 1e-5f);
+  ASSERT_NEAR(fft.s.b[1].real(), 1.55030f, 1e-5f);
+  ASSERT_NEAR(fft.s.b[1].imag(), -1.61139f, 1e-5f);
+  ASSERT_NEAR(fft.s.b[2].real(), 0.92014f, 1e-5f);
+  ASSERT_NEAR(fft.s.b[2].imag(), -0.39159f, 1e-5f);
   fft.ifft();
-  ASSERT_NEAR(2.15536f,fft.w.d[0],1e-5f);
-  ASSERT_NEAR(1.72583f,fft.w.d[1],1e-5f);
-  ASSERT_NEAR(0.60506f,fft.w.d[2],1e-5f);
-  ASSERT_NEAR(0.11444f,fft.w.d[3],1e-5f);
+  ASSERT_NEAR(2.15536f, fft.w.d[0], 1e-5f);
+  ASSERT_NEAR(1.72583f, fft.w.d[1], 1e-5f);
+  ASSERT_NEAR(0.60506f, fft.w.d[2], 1e-5f);
+  ASSERT_NEAR(0.11444f, fft.w.d[3], 1e-5f);
 }
 
 TEST(get_bandlevels, bandspacing)
@@ -124,14 +124,14 @@ TEST(get_bandlevels, bandspacing)
   TASCAR::wave_t w(2000);
   std::vector<float> vF;
   std::vector<float> vL;
-  TASCAR::get_bandlevels(w, 100.0f, 400.0f, 2000.0f, 1.0f, vF, vL);
+  TASCAR::get_bandlevels(w, 100.0f, 400.0f, 2000.0f, 1.0f, 0.0f, vF, vL);
   ASSERT_EQ(vF.size(), 3u);
   if(vF.size() == 3) {
     ASSERT_NEAR(vF[0], 100.0f, 0.1f);
     ASSERT_NEAR(vF[1], 200.0f, 0.1f);
     ASSERT_NEAR(vF[2], 400.0f, 0.1f);
   }
-  TASCAR::get_bandlevels(w, 100.0f, 400.0f, 2000.0f, 3.0f, vF, vL);
+  TASCAR::get_bandlevels(w, 100.0f, 400.0f, 2000.0f, 3.0f, 0.0f, vF, vL);
   ASSERT_EQ(vF.size(), 7u);
   if(vF.size() == 3) {
     ASSERT_NEAR(vF[0], 100.0f, 0.1f);
@@ -149,12 +149,12 @@ TEST(get_bandlevels, bandlevels)
   float fs = 2000.0f;
   float f0 = 200.0f;
   TASCAR::wave_t w((uint32_t)fs);
-  for(size_t k=0;k<w.n;++k)
-    w.d[k] = sinf((float)k/fs*TASCAR_2PIf*f0);
+  for(size_t k = 0; k < w.n; ++k)
+    w.d[k] = sinf((float)k / fs * TASCAR_2PIf * f0);
   ASSERT_NEAR(w.spldb(), 91.0f, 0.1f);
   std::vector<float> vF;
   std::vector<float> vL;
-  TASCAR::get_bandlevels(w, 100.0f, 400.0f, fs, 1.0f, vF, vL);
+  TASCAR::get_bandlevels(w, 100.0f, 400.0f, fs, 1.0f, 0.0f, vF, vL);
   ASSERT_EQ(vL.size(), 3u);
   if(vL.size() == 3) {
     ASSERT_NEAR(vL[0], -21.0f, 12.0f);
@@ -168,12 +168,12 @@ TEST(get_bandlevels, bandlevels2)
   float fs = 8000.0f;
   float f0 = 200.0f;
   TASCAR::wave_t w((uint32_t)fs);
-  for(size_t k=0;k<w.n;++k)
-    w.d[k] = sinf((float)k/fs*TASCAR_2PIf*f0);
+  for(size_t k = 0; k < w.n; ++k)
+    w.d[k] = sinf((float)k / fs * TASCAR_2PIf * f0);
   ASSERT_NEAR(w.spldb(), 91.0f, 0.1f);
   std::vector<float> vF;
   std::vector<float> vL;
-  TASCAR::get_bandlevels(w, 100.0f, 400.0f, fs, 1.0f, vF, vL);
+  TASCAR::get_bandlevels(w, 100.0f, 400.0f, fs, 1.0f, 0.0f, vF, vL);
   ASSERT_EQ(vL.size(), 3u);
   if(vL.size() == 3u) {
     ASSERT_NEAR(vL[0], -25.0f, 12.0f);
@@ -186,12 +186,12 @@ TEST(get_bandlevels, bandlevels3)
 {
   float fs = 8000.0f;
   TASCAR::wave_t w((uint32_t)fs);
-  for(size_t k=0;k<w.n;++k)
-    w.d[k] = (float)TASCAR::drand()-0.5f;
+  for(size_t k = 0; k < w.n; ++k)
+    w.d[k] = (float)TASCAR::drand() - 0.5f;
   ASSERT_NEAR(w.spldb(), 83.2f, 0.5f);
   std::vector<float> vF;
   std::vector<float> vL;
-  TASCAR::get_bandlevels(w, 50.0f, 3200.0f, fs, 1.0f, vF, vL);
+  TASCAR::get_bandlevels(w, 50.0f, 3200.0f, fs, 1.0f, 0.0f, vF, vL);
   ASSERT_EQ(vL.size(), 7u);
   if(vL.size() == 7u) {
     ASSERT_NEAR(vL[0], 61.4f, 2.0f);
