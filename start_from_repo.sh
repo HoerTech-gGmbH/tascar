15f8f16919bf61f742e1cbc48147ac980f04fc3a
Giso Grimm <giso@Gisos-Mac-mini.local>
2022-11-04 09:35:31 +0100

    make startfromrepo macos compatible

--- start_from_repo.sh
+++ start_from_repo.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-LOCTSC=$(pwd)/$(dirname $0)
-echo $LOCTSC
-LOCTSCLIB="${LOCTSC}/libtascar/build:${LOCTSC}/plugins/build"
-LD_LIBRARY_PATH="${LOCTSCLIB}" $*
+BASE=$(realpath $(dirname $0))
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BASE}/libtascar/build:${BASE}/plugins/build \
+	       DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${BASE}/libtascar/build:${BASE}/plugins/build \
+	       PATH=$PATH:${BASE}/apps/build $*
+#LOCTSC=$(pwd)/$(dirname $0)
+#echo $LOCTSC
+#LOCTSCLIB="${LOCTSC}/libtascar/build:${LOCTSC}/plugins/build"
+#LD_LIBRARY_PATH="${LOCTSCLIB}" $*
