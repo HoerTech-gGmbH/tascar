20707232d0a611bca2d5fbbab90bd2f829967985
Giso Grimm <giso.grimm@vegri.net>
2022-11-16 23:34:33 +0100

    show serial port flags

--- libtascar/src/serialport.cc
+++ libtascar/src/serialport.cc
@@ -28,11 +28,14 @@
 
 using namespace TASCAR;
 
-void test_iflag(std::string& str, int flag)
-{
 #define TESTFLAG(x, y)                                                         \
   if(x & y)                                                                    \
   str += std::string(#y) + std::string(" ")
+
+std::string test_iflag(int flag)
+{
+  std::string str = std::to_string(flag);
+  str += "\n";
   TESTFLAG(flag, IGNBRK);
   TESTFLAG(flag, BRKINT);
   TESTFLAG(flag, IGNPAR);
@@ -48,6 +51,86 @@ void test_iflag(std::string& str, int flag)
   TESTFLAG(flag, IXOFF);
   TESTFLAG(flag, IMAXBEL);
   TESTFLAG(flag, IUTF8);
+  return str;
+}
+
+std::string test_lflag(int flag)
+{
+  std::string str = std::to_string(flag);
+  str += "\n";
+  TESTFLAG(flag, ISIG);
+  TESTFLAG(flag, ICANON);
+  TESTFLAG(flag, XCASE);
+  TESTFLAG(flag, ECHO);
+  TESTFLAG(flag, ECHOE);
+  TESTFLAG(flag, ECHOK);
+  TESTFLAG(flag, ECHONL);
+  TESTFLAG(flag, NOFLSH);
+  TESTFLAG(flag, TOSTOP);
+  TESTFLAG(flag, ECHOCTL);
+  TESTFLAG(flag, ECHOPRT);
+  TESTFLAG(flag, ECHOKE);
+  TESTFLAG(flag, FLUSHO);
+  TESTFLAG(flag, PENDIN);
+  TESTFLAG(flag, IEXTEN);
+  TESTFLAG(flag, EXTPROC);
+  return str;
+}
+
+#define TESTBAUD(flag, y)                                                      \
+  if((flag & CBAUD) == y)                                                      \
+  str += std::string(#y) + std::string(" ")
+
+std::string test_cflag(int flag)
+{
+  std::string str = std::to_string(flag);
+  str += "\n";
+
+  TESTBAUD(flag, B0);
+  TESTBAUD(flag, B50);
+  TESTBAUD(flag, B75);
+  TESTBAUD(flag, B110);
+  TESTBAUD(flag, B134);
+  TESTBAUD(flag, B150);
+  TESTBAUD(flag, B200);
+  TESTBAUD(flag, B300);
+  TESTBAUD(flag, B600);
+  TESTBAUD(flag, B1200);
+  TESTBAUD(flag, B1800);
+  TESTBAUD(flag, B2400);
+  TESTBAUD(flag, B4800);
+  TESTBAUD(flag, B9600);
+  TESTBAUD(flag, B19200);
+  TESTBAUD(flag, B38400);
+  TESTBAUD(flag, B115200);
+  TESTBAUD(flag, B230400);
+  TESTBAUD(flag, B460800);
+  TESTBAUD(flag, B500000);
+  TESTBAUD(flag, B576000);
+  TESTBAUD(flag, B921600);
+  TESTBAUD(flag, B1000000);
+  TESTBAUD(flag, B1152000);
+  TESTBAUD(flag, B1500000);
+  TESTBAUD(flag, B2000000);
+  TESTBAUD(flag, B2500000);
+  TESTBAUD(flag, B3000000);
+  TESTBAUD(flag, B3500000);
+  TESTBAUD(flag, B4000000);
+  // TESTFLAG(flag, CSIZE);
+  // TESTFLAG(flag, CS5);
+  // TESTFLAG(flag, CS6);
+  // TESTFLAG(flag, CS7);
+  // TESTFLAG(flag, CS8);
+  TESTFLAG(flag, CSTOPB);
+  TESTFLAG(flag, CREAD);
+  TESTFLAG(flag, PARENB);
+  TESTFLAG(flag, PARODD);
+  TESTFLAG(flag, HUPCL);
+  TESTFLAG(flag, CLOCAL);
+
+  TESTFLAG(flag, CMSPAR);
+  TESTFLAG(flag, CRTSCTS);
+  return str;
 }
 
 serialport_t::serialport_t() : fd(-1) {}
@@ -93,20 +176,20 @@ void serialport_t::set_interface_attribs(int speed, int parity, int stopbits,
   DEBUG(tty.c_iflag);
   DEBUG(tty.c_oflag);
   DEBUG(tty.c_lflag);
+  DEBUG(test_iflag(tty.c_iflag));
+  DEBUG(test_lflag(tty.c_lflag));
+  DEBUG(test_cflag(tty.c_cflag));
   if(!xbaud) {
     cfsetospeed(&tty, speed);
     cfsetispeed(&tty, speed);
   }
-  std::string sflag = "";
-  test_iflag(sflag, tty.c_iflag);
-  DEBUG(sflag);
   tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
   // disable IGNBRK for mismatched speed tests; otherwise receive break
   // as \000 chars
   tty.c_iflag &= ~IGNBRK; // disable break processing
   tty.c_iflag |= BRKINT;  // enable break processing
   tty.c_iflag |= ICRNL;
-  tty.c_lflag = 0;        // no signaling chars, no echo,
+  tty.c_lflag = 0; // no signaling chars, no echo,
   // no canonical processing
   tty.c_oflag = 0; // no remapping, no delays
 #ifdef ISMACOS
@@ -127,9 +210,9 @@ void serialport_t::set_interface_attribs(int speed, int parity, int stopbits,
   DEBUG(tty.c_iflag);
   DEBUG(tty.c_oflag);
   DEBUG(tty.c_lflag);
-  sflag = "";
-  test_iflag(sflag, tty.c_iflag);
-  DEBUG(sflag);
+  DEBUG(test_iflag(tty.c_iflag));
+  DEBUG(test_lflag(tty.c_lflag));
+  DEBUG(test_cflag(tty.c_cflag));
   if(tcsetattr(fd, TCSANOW, &tty) != 0)
     throw TASCAR::ErrMsg("error from tcsetattr");
   int flags;
