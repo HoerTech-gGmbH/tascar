064a1a1dd8991e3cab1d61579a89bd94e0fbd762
Giso Grimm <g.grimm@uni-oldenburg.de>
2023-01-12 09:42:54 +0100

    documentation of the air absorption filter

--- scripts/airabsorptionfilter.m
+++ scripts/airabsorptionfilter.m
@@ -1,4 +1,17 @@
 function [B,A] = airabsorptionfilter( d, fs, c, alpha )
+% airabsorptionfilter - calculate distance-dependent air absorption filter coefficients
+%
+% d : distance in m
+% fs: sampling rate in Hz (default: 44100)
+% c : speed of sound in m/s (default: 340)
+% alpha: scaling variable, dimensionless (default: 7782)
+%
+% Example:
+% Create filtered white noise in 100m distance:
+% x = rand(44100,1)-0.5;
+% [B,A] = airabsorptionfilter( 100 );
+% y = filter(B, A, x);
+
   if nargin < 4
     alpha = 7782;
   end
