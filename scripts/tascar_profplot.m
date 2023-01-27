4eb0da48a0d327a2779ee441b2fe9cae76cc9664
Giso Grimm <g.grimm@uni-oldenburg.de>
2023-01-27 10:57:45 +0100

    correct profiling plot

--- scripts/tascar_profplot.m
+++ scripts/tascar_profplot.m
@@ -7,7 +7,7 @@ if nargin < 2
 end
 data = data(2:end,:)';
 tmax = min(max(data(:)),20*median(data(:)));
-dt = max(tmax/100,1e-6);
+dt = max(tmax/256,1e-6);
 edges = 0:dt:tmax;
 [h,bins] = hist(data,edges);
 h = h/size(data,1);
@@ -19,7 +19,15 @@ ylabel('relative frequency');
 title(['cycle time histogram, ',num2str(size(data,1)),' samples']);
 legend(labels);
 figure
-barh(1e6*max(data));
-set(gca,'YTick',1:numel(labels),'YTickLabels',labels,'YDir','reverse');
-title('maximum cycle time');
+barh(1e6*mean(data));
+hold on;
+for k=1:size(data,2)
+  plot([0,1e6]*max(data(:,k)),[k,k],'k-','linewidth',2);
+  plot([1e6,1e6]*max(data(:,k)),[k-0.2,k+0.2],'k-','linewidth',2);
+end
+set(gca,'YTick',1:numel(labels),'YTickLabel',labels,'YDir','reverse',...
+   'YLim',[0.5,0.5+size(data,2)]);
+title('average/maximum cycle time');
 xlabel('time per cycle / microseconds');
+end
+
