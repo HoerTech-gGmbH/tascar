d722ecb61bb7f87c66f2a532bd8ca7ab1086a556
Giso Grimm <giso.grimm@vegri.net>
2025-05-29 17:52:22 +0200

    rename variables for ovbox compatibility

--- plugins/src/tascar_ap_pareq4band.cc
+++ plugins/src/tascar_ap_pareq4band.cc
@@ -35,69 +35,70 @@ public:
   ~pareq4band_t();
 
 private:
-  float b1f = 150.0;
-  float b2f = 560.0;
-  float b3f = 1500.0;
-  float b4f = 5600.0;
-  float b1Q = 1.0;
-  float b2Q = 1.0;
-  float b3Q = 1.0;
-  float b4Q = 1.0;
-  float b1g = 0.0;
-  float b2g = 0.0;
-  float b3g = 0.0;
-  float b4g = 0.0;
-  bool b1active = true;
-  bool b2active = true;
-  bool b3active = true;
-  bool b4active = true;
-  std::vector<TASCAR::biquadf_t*> b1flt;
-  std::vector<TASCAR::biquadf_t*> b2flt;
-  std::vector<TASCAR::biquadf_t*> b3flt;
-  std::vector<TASCAR::biquadf_t*> b4flt;
+  float b1ls_f = 150.0;
+  float b2eq_f = 560.0;
+  float b3eq_f = 1500.0;
+  float b4hs_f = 5600.0;
+  float b1ls_Q = 1.0;
+  float b2eq_Q = 1.0;
+  float b3eq_Q = 1.0;
+  float b4hs_Q = 1.0;
+  float b1ls_g = 0.0;
+  float b2eq_g = 0.0;
+  float b3eq_g = 0.0;
+  float b4hs_g = 0.0;
+  bool b1ls_act = true;
+  bool b2eq_act = true;
+  bool b3eq_act = true;
+  bool b4hs_act = true;
+  std::vector<TASCAR::biquadf_t*> b1ls_flt;
+  std::vector<TASCAR::biquadf_t*> b2eq_flt;
+  std::vector<TASCAR::biquadf_t*> b3eq_flt;
+  std::vector<TASCAR::biquadf_t*> b4hs_flt;
 };
 
 pareq4band_t::pareq4band_t(const TASCAR::audioplugin_cfg_t& cfg)
     : audioplugin_base_t(cfg)
 {
-  GET_ATTRIBUTE(b1f, "Hz", "low-shelf frequency 22-560");
-  GET_ATTRIBUTE(b2f, "Hz", "mid1 eq frequency 150-1.5k");
-  GET_ATTRIBUTE(b3f, "Hz", "mid2 eq frequency 560-5.6k");
-  GET_ATTRIBUTE(b4f, "Hz", "high-shelf frequency 5.6k-24k");
-  GET_ATTRIBUTE(b1g, "dB", "low-shelf gain -20,20");
-  GET_ATTRIBUTE(b2g, "dB", "mid1 eq gain");
-  GET_ATTRIBUTE(b3g, "dB", "mid2 eq gain");
-  GET_ATTRIBUTE(b4g, "dB", "high-shelf gain");
-  GET_ATTRIBUTE(b1Q, "", "slope");
-  GET_ATTRIBUTE(b2Q, "", "bandwidth");
-  GET_ATTRIBUTE(b3Q, "", "bandwidth");
-  GET_ATTRIBUTE(b4Q, "", "slope");
-  GET_ATTRIBUTE_BOOL(b1active, "activate");
-  GET_ATTRIBUTE_BOOL(b2active, "activate");
-  GET_ATTRIBUTE_BOOL(b3active, "activate");
-  GET_ATTRIBUTE_BOOL(b4active, "activate");
+  GET_ATTRIBUTE(b1ls_f, "Hz", "low-shelf frequency 22-560");
+  GET_ATTRIBUTE(b2eq_f, "Hz", "mid1 eq frequency 150-1.5k");
+  GET_ATTRIBUTE(b3eq_f, "Hz", "mid2 eq frequency 560-5.6k");
+  GET_ATTRIBUTE(b4hs_f, "Hz", "high-shelf frequency 5.6k-24k");
+  GET_ATTRIBUTE(b1ls_g, "dB", "low-shelf gain -20,20");
+  GET_ATTRIBUTE(b2eq_g, "dB", "mid1 eq gain");
+  GET_ATTRIBUTE(b3eq_g, "dB", "mid2 eq gain");
+  GET_ATTRIBUTE(b4hs_g, "dB", "high-shelf gain");
+  GET_ATTRIBUTE(b1ls_Q, "", "slope");
+  GET_ATTRIBUTE(b2eq_Q, "", "bandwidth");
+  GET_ATTRIBUTE(b3eq_Q, "", "bandwidth");
+  GET_ATTRIBUTE(b4hs_Q, "", "slope");
+  GET_ATTRIBUTE_BOOL(b1ls_act, "activate");
+  GET_ATTRIBUTE_BOOL(b2eq_act, "activate");
+  GET_ATTRIBUTE_BOOL(b3eq_act, "activate");
+  GET_ATTRIBUTE_BOOL(b4hs_act, "activate");
 }
 
 void pareq4band_t::add_variables(TASCAR::osc_server_t* srv)
 {
   srv->set_variable_owner(
       TASCAR::strrep(TASCAR::tscbasename(__FILE__), ".cc", ""));
-  srv->add_float("/1/f", &b1f, "[22,560]", "low-shelf frequency in Hz");
-  srv->add_float("/2/f", &b2f, "[150,1500]", "mid1 eq frequency in Hz");
-  srv->add_float("/3/f", &b3f, "[560,5600]", "mid2 eq frequency in Hz");
-  srv->add_float("/4/f", &b4f, "[1500,24000]", "high-shelf frequency in Hz");
-  srv->add_float("/1/g", &b1g, "[-20,20]", "Gain in dB");
-  srv->add_float("/2/g", &b2g, "[-20,20]", "Gain in dB");
-  srv->add_float("/3/g", &b3g, "[-20,20]", "Gain in dB");
-  srv->add_float("/4/g", &b4g, "[-20,20]", "Gain in dB");
-  srv->add_float("/1/q", &b1Q, "[0.1,10]", "Q-factor");
-  srv->add_float("/2/q", &b2Q, "[0.1,10]", "Q-factor");
-  srv->add_float("/3/q", &b3Q, "[0.1,10]", "Q-factor");
-  srv->add_float("/4/q", &b4Q, "[0.1,10]", "Q-factor");
-  srv->add_bool("/1/act", &b1active, "active");
-  srv->add_bool("/2/act", &b2active, "active");
-  srv->add_bool("/3/act", &b3active, "active");
-  srv->add_bool("/4/act", &b4active, "active");
+  srv->add_float("/b1ls_f", &b1ls_f, "[22,560]", "low-shelf frequency in Hz");
+  srv->add_float("/b2eq_f", &b2eq_f, "[150,1500]", "mid1 eq frequency in Hz");
+  srv->add_float("/b3eq_f", &b3eq_f, "[560,5600]", "mid2 eq frequency in Hz");
+  srv->add_float("/b4hs_f", &b4hs_f, "[1500,24000]",
+                 "high-shelf frequency in Hz");
+  srv->add_float("/b1ls_g", &b1ls_g, "[-20,20]", "Gain in dB");
+  srv->add_float("/b2eq_g", &b2eq_g, "[-20,20]", "Gain in dB");
+  srv->add_float("/b3eq_g", &b3eq_g, "[-20,20]", "Gain in dB");
+  srv->add_float("/b4hs_g", &b4hs_g, "[-20,20]", "Gain in dB");
+  srv->add_float("/b1ls_q", &b1ls_Q, "[0.1,10]", "Q-factor");
+  srv->add_float("/b2eq_q", &b2eq_Q, "[0.1,10]", "Q-factor");
+  srv->add_float("/b3eq_q", &b3eq_Q, "[0.1,10]", "Q-factor");
+  srv->add_float("/b4hs_q", &b4hs_Q, "[0.1,10]", "Q-factor");
+  srv->add_bool("/b1ls_act", &b1ls_act, "active");
+  srv->add_bool("/b2eq_act", &b2eq_act, "active");
+  srv->add_bool("/b3eq_act", &b3eq_act, "active");
+  srv->add_bool("/b4hs_act", &b4hs_act, "active");
   srv->unset_variable_owner();
 }
 
@@ -105,52 +106,52 @@ void pareq4band_t::configure()
 {
   audioplugin_base_t::configure();
   for(uint32_t ch = 0; ch < n_channels; ++ch) {
-    b1flt.push_back(new TASCAR::biquadf_t());
-    b2flt.push_back(new TASCAR::biquadf_t());
-    b3flt.push_back(new TASCAR::biquadf_t());
-    b4flt.push_back(new TASCAR::biquadf_t());
+    b1ls_flt.push_back(new TASCAR::biquadf_t());
+    b2eq_flt.push_back(new TASCAR::biquadf_t());
+    b3eq_flt.push_back(new TASCAR::biquadf_t());
+    b4hs_flt.push_back(new TASCAR::biquadf_t());
   }
 }
 
 void pareq4band_t::release()
 {
   audioplugin_base_t::release();
-  for(auto& b1f : b1flt)
-    delete(b1f);
-  b1flt.clear();
-  for(auto& b2f : b2flt)
-    delete(b2f);
-  b2flt.clear();
-  for(auto& b3f : b3flt)
-    delete(b3f);
-  b3flt.clear();
-  for(auto& b4f : b4flt)
-    delete(b4f);
-  b4flt.clear();
+  for(auto& b1ls_f : b1ls_flt)
+    delete(b1ls_f);
+  b1ls_flt.clear();
+  for(auto& b2eq_f : b2eq_flt)
+    delete(b2eq_f);
+  b2eq_flt.clear();
+  for(auto& b3eq_f : b3eq_flt)
+    delete(b3eq_f);
+  b3eq_flt.clear();
+  for(auto& b4hs_f : b4hs_flt)
+    delete(b4hs_f);
+  b4hs_flt.clear();
 }
 
 pareq4band_t::~pareq4band_t() {}
 
 void pareq4band_t::ap_process(std::vector<TASCAR::wave_t>& chunk,
-                            const TASCAR::pos_t&, const TASCAR::zyx_euler_t&,
-                            const TASCAR::transport_t&)
+                              const TASCAR::pos_t&, const TASCAR::zyx_euler_t&,
+                              const TASCAR::transport_t&)
 {
   for(size_t k = 0; k < chunk.size(); ++k) {
-    if(b1active) {
-      b1flt[k]->set_lowshelf(b1f, (float)f_sample, b1g, b1Q);
-      b1flt[k]->filter(chunk[k]);
+    if(b1ls_act) {
+      b1ls_flt[k]->set_lowshelf(b1ls_f, (float)f_sample, b1ls_g, b1ls_Q);
+      b1ls_flt[k]->filter(chunk[k]);
     }
-    if(b2active) {
-      b2flt[k]->set_pareq(b2f, (float)f_sample, b2g, b2Q);
-      b2flt[k]->filter(chunk[k]);
+    if(b2eq_act) {
+      b2eq_flt[k]->set_pareq(b2eq_f, (float)f_sample, b2eq_g, b2eq_Q);
+      b2eq_flt[k]->filter(chunk[k]);
     }
-    if(b3active) {
-      b3flt[k]->set_pareq(b3f, (float)f_sample, b3g, b3Q);
-      b3flt[k]->filter(chunk[k]);
+    if(b3eq_act) {
+      b3eq_flt[k]->set_pareq(b3eq_f, (float)f_sample, b3eq_g, b3eq_Q);
+      b3eq_flt[k]->filter(chunk[k]);
     }
-    if(b4active) {
-      b4flt[k]->set_highshelf(b4f, (float)f_sample, b4g, b4Q);
-      b4flt[k]->filter(chunk[k]);
+    if(b4hs_act) {
+      b4hs_flt[k]->set_highshelf(b4hs_f, (float)f_sample, b4hs_g, b4hs_Q);
+      b4hs_flt[k]->filter(chunk[k]);
     }
   }
 }
