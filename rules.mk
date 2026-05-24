6e4d2bc35acea8b973ee937ad693f55623fd6f89
Christopher Obbard <obbardc@debian.org>
2026-05-24 20:41:39 +0100

    build: pass CPPFLAGS through to all compile and preprocess invocations
    Add $(CPPFLAGS) to every compile, preprocess and link rule across
    apps/, devkit/, libtascar/, plugins/, gui/ and rules.mk so that the
    Debian-supplied preprocessor flags (e.g. hardening defines from
    dpkg-buildflags) reach the compiler. Also link the libtascar test_%
    binaries with LDFLAGS and LDLIBS, which were previously missing.
    
    Signed-off-by: Christopher Obbard <obbardc@debian.org>

--- rules.mk
+++ rules.mk
@@ -70,4 +70,4 @@ GTEST_LIBS = -lgmock_main -lgmock -lgtest_main -lgtest -lpthread
 endif
 
 $(BUILD_DIR)/unit-test-runner: $(BUILD_DIR)/.directory $(unit_tests_test_files) $(patsubst %_unit_tests.cpp, %.cpp , $(unit_tests_test_files))
-	if test -n "$(unit_tests_test_files)"; then $(CXX) $(CXXFLAGS) --coverage -o $@ $(wordlist 2, $(words $^), $^) $(LDFLAGS) $(LIBTASCARDLL) $(LDLIBS) $(GTEST_LIBPATH) $(GTEST_LIBS); fi
+	if test -n "$(unit_tests_test_files)"; then $(CXX) $(CPPFLAGS) $(CXXFLAGS) --coverage -o $@ $(wordlist 2, $(words $^), $^) $(LDFLAGS) $(LIBTASCARDLL) $(LDLIBS) $(GTEST_LIBPATH) $(GTEST_LIBS); fi
